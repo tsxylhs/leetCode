986e59a4277e11cfb23153e01eacac0302a57fbd
lhs <tsxylhs@qq.com>
2021-06-17 10:15:40 +0800

    下一个大的值

---
+++ leetcode/nextGreaterElement.go
@@ -0,0 +1,51 @@
+package leetcode
+
+// 下一个元素更大
+// 给你两个 没有重复元素 的数组 nums1 和 nums2 ，其中nums1 是 nums2 的子集。
+//
+//请你找出 nums1 中每个元素在 nums2 中的下一个比其大的值。
+//
+//nums1 中数字 x 的下一个更大元素是指 x 在 nums2 中对应位置的右边的第一个比 x 大的元素。如果不存在，对应位置输出 -1 。
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/next-greater-element-i
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+func nextGreaterElement(nums1 []int, nums2 []int) []int {
+	retVal := make([]int, 0)
+	map2 := make(map[int]int, 0)
+	for i, v := range nums2 {
+		map2[i] = v
+	}
+	for i1, v1 := range nums1 {
+		for i2, v2 := range map2 {
+			if map2[i1+1] > v1 {
+				retVal = append(retVal, map2[i1])
+			} else {
+				retVal = append(retVal, -1)
+			}
+		}
+	}
+	return retVal
+}
+
+func NextGreaterElement(nums1 []int, nums2 []int) []int {
+	ret := make([]int, 0)
+	stack := make([]int, 0)
+	hm := make(map[int]int)
+
+	for i := len(nums2) - 1; i >= 0; i-- {
+		for len(stack) != 0 && stack[len(stack)-1] <= nums2[i] {
+			stack = stack[:len(stack)-1]
+		}
+		if len(stack) == 0 {
+			hm[nums2[i]] = -1
+		} else {
+			hm[nums2[i]] = stack[len(stack)-1]
+		}
+		stack = append(stack, nums2[i])
+	}
+	for _, v := range nums1 {
+		ret = append(ret, hm[v])
+	}
+	return ret
+}
